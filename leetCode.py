602642f502a326865b922c5cf89dbdb080c33e2d
Farhan Hamzah <177802321+farhan-hamzah@users.noreply.github.com>
2025-02-20 20:19:58 +0700

    Add files via upload

---
+++ leetCode.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(inpit())
+
+if (a > b && a < c) or (a > c && a < b):
+    fmt.Println(a)
+elif (b > a && b < c) or (b > c && b < a) :
+    fmt.Println(b)
+else :
+    fmt.Println(c)
